28911009182f862fe3b833d0ebcfd70aea2f3699
Travis Smith <t@wpsmith.net>
2013-11-07 10:57:35 -0500

    Added Plugin Action Links

--- gs-featured-content-widget.php
+++ gs-featured-content-widget.php
@@ -63,6 +63,10 @@ function gsfc_activation_check() {
 }
 
 add_action( 'genesis_init', 'gsfc_init', 50 );
+/**
+ * Initializes Widget & Admin Settings
+ * @since 1.1.0
+ */
 function gsfc_init() {
     require_once( 'widget.php' );
     require_once( 'gsfc-settings.php' );
@@ -70,4 +74,21 @@ function gsfc_init() {
     global $_gsfc_settings;
     $_gsfc_settings = new GSFC_Settings();
     
+}
+
+add_filter( 'plugin_action_links', 'gsfc_action_links', 10, 2 );
+/**
+ * Add Menus & Donate Action Link.
+ * 
+ * @param array $links Array of links.
+ * @param string $file Basename of plugin.
+ * @return array $links Maybe modified array of links.
+ */
+function gsfc_action_links( $links, $file ) {
+    if ( $file == WPS_WPCM_PLUGIN_FILE ) {
+        array_unshift( $links, sprintf( '<a href="%s">%s</a>', admin_url( 'admin.php?page=genesis' ), __( 'Settings', 'gsfc' ) ) );
+        array_unshift( $links, sprintf( '<a href="%s">%s</a>', admin_url( 'widgets.php' ), __( 'Widgets', 'gsfc' ) ) );
+        array_push( $links, sprintf( '<a href="http://wpsmith.net/donation" target="_blank">%s</a>', __( 'Donate', 'gsfc' ) ) );
+    }
+    return $links;
 }
