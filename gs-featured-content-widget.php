f9810add8d702e9eb7d783ce20e5fc7e40663f48
David Green <david@shycyborg.com>
2015-03-01 15:02:27 -0600

    Solves issue #43. #37 and #38 are the same error. Instead of a fatal error when the plugin is activated without Genesis active, it will deactivate the plugin and show an admin notice. If a non-Genesis theme is activated while the plugin is already activated then it will also deactivate itself and notify the user.

--- gs-featured-content-widget.php
+++ gs-featured-content-widget.php
@@ -36,25 +36,53 @@ define( 'GSFC_PLUGIN_VERSION', '1.1.0' );
 /** Load textdomain for translation */
 load_plugin_textdomain( 'gsfc', false, basename( dirname( __FILE__ ) ) . '/languages/' );
 
-// register_activation_hook( __FILE__, 'gsfc_activation_check' );
+/**
+ * Add admin notice notifying user that Genesis is required
+ */
+function gsfc_notify_genesis_required() {
+
+    echo '<div class="error below-h2"><p><strong>Sorry, but the Genesis Sandbox Featured Post Widget plugin cannot be activated without Genesis or a Genesis child theme already activated.</strong></p></div>';
+
+    // Prevent the Plugin Activated message from appearing
+    if ( isset( $_GET['activate'] ) ) {
+        unset( $_GET['activate'] );
+    }
+}
+
+/**
+ * Called by admin_init hook if plugin needs deactivated due
+ * to not meeting requirements.
+ */
+function gsfc_plugin_deactivate() {
+    deactivate_plugins( plugin_basename( __FILE__ ) );
+}
+
+register_activation_hook( __FILE__, 'gsfc_activation_check' );
 /**
  * Checks for minimum Genesis Theme version before allowing plugin to activate
  *
  * @uses genesis_truncate_phrase()
+ * @return bool Indicate whether plugin was activated
  */
 function gsfc_activation_check() {
 
     $latest = '2.0';
 
     if ( basename( get_template_directory() ) != 'genesis' ) {
-        deactivate_plugins( plugin_basename( __FILE__ ) ); // Deactivate ourself
-        wp_die( sprintf( __( 'Sorry, you can\'t activate unless you have installed %1$sGenesis%2$s', 'gsfc' ), '<a href="http://wpsmith.net/get-genesis/">', '</a>' ) );
+        // Deactivate the plugin and send a notice to the user
+        add_action( 'admin_init', 'gsfc_plugin_deactivate' );
+        add_action( 'admin_notices', 'gsfc_notify_genesis_required' );
+        return false;
     }
 
-    if ( ! function_exists( 'genesis_upgrade_2001' ) ) {
-        deactivate_plugins( plugin_basename( __FILE__ ) ); // Deactivate ourself
-        wp_die( sprintf( __( 'Sorry, you can\'t activate without %1$sGenesis %2$s%3$s or greater', 'gsfc' ), '<a href="http://wpsmith.net/get-genesis/">', $latest, '</a>' ) );
+    if ( ! function_exists( 'genesis_upgrade_2001' ) || ! function_exists( 'genesis_get_option' ) ) {
+        // Deactivate the plugin and send a notice to the user
+        add_action( 'admin_init', 'gsfc_plugin_deactivate' );
+        add_action( 'admin_notices', 'gsfc_notify_genesis_required' );
+        return false;
     }
+
+    return true;
 }
 
 add_action( 'genesis_init', 'gsfc_init', 50 );
@@ -80,6 +108,9 @@ add_action( 'widgets_init', 'gsfc_widgets_init', 50 );
  * @since 1.1.0
  */
 function gsfc_widgets_init() {
+    if( gsfc_activation_check() === false ) {
+        return;
+    }
     if ( class_exists( 'Premise_Base' ) && !is_admin() ) {
         return;
     }
